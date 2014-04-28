586ad28cc053d69b78fb8ad30205511cba3766c4
Tony Tam <tonytam@yahoo-inc.com>
2014-04-28 10:02:10 -0700

    layout

--- Get To 24/Get To 24/GetTo24ViewController.m
+++ Get To 24/Get To 24/GetTo24ViewController.m
@@ -19,6 +19,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.view setMultipleTouchEnabled:YES];
 }
 
 - (void)didReceiveMemoryWarning
